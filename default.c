af6229168fbe9067b7b0e03c2f8c1640dac106e1
Gpap Peaky <150977755+GpapPeaky@users.noreply.github.com>
2023-12-10 03:14:20 +0200

    Added default library for general use functions
    new_scope(); to separate blocks of code

---
+++ default.c
@@ -0,0 +1,19 @@
+#include "default.h"
+
+void new_scope(void){
+
+    for(int i = 0 ; i < 60 ; i++){
+        printf("%c",196);
+    }
+    printf("\n");
+
+    return;
+}
+
+void dovas(void){
+
+    for(int i = 0 ; i < 69 ; i++){
+        printf("1 Row Created.\n");
+    }
+    return;
+}
---
+++ default.h
@@ -0,0 +1,27 @@
+/**
+ * @author Gpap Peaky
+ * 
+ * @brief Simple C - library for learning purposes
+ * 
+ * @version 29.11.23
+ * 
+ * This Library Serves Only Prints
+*/
+
+/*___AUXILARY MACROS___*/
+
+#include "../_macros.h"
+
+/**
+ * New Scope Block
+ * 
+ * Used For Optimised Printing
+*/
+void new_scope(void);
+
+/**
+ * Debug Print Function
+ * 
+ * Used For Triggering Dovas
+*/
+void dovas(void);
